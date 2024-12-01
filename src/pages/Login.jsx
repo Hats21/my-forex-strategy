6dd208930fb1c09021fb56e8cd6a9c2ef47af392
Hatsey Tikur <hatseytikur@gmail.com>
2024-12-01 23:10:05 +0300

    changing the background of the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import LoginForm from "../features/authentication/LoginForm";
-import Logo from "../ui/Logo";
-import Heading from "../ui/Heading";
+// import Logo from "../ui/Logo";
 import { useNavigate } from "react-router-dom";
 import { useUser } from "../features/authentication/useUser";
 import { useEffect } from "react";
@@ -14,6 +13,8 @@ const LoginLayout = styled.main`
   justify-content: center;
   gap: 3.2rem;
   background-color: var(--color-grey-50);
+  background-image: url("/img/bg-login.png");
+  background-size: cover;
 `;
 
 function Login() {
@@ -29,8 +30,6 @@ function Login() {
 
   return (
     <LoginLayout>
-      <Logo />
-      <Heading as="h4">Log in to your account</Heading>
       <LoginForm />
     </LoginLayout>
   );
